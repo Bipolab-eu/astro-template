2267ca11fa4a54b8b6dbc7f737b053ccd891143a
bipolab-eu <bentagai@bipolab.eu>
2025-05-09 17:40:06 +0100

    instala sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   i18n: {
     locales: ["es", "en"],
