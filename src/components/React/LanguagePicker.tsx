36254d170927b6cc5b50fe3ab9edfac4bf17c319
bipolab-eu <bentagai@bipolab.eu>
2025-05-12 17:22:55 +0100

    mejoras ui

--- src/components/React/LanguagePicker.tsx
+++ src/components/React/LanguagePicker.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { languages } from '../../i18n/ui'
 import { useTranslatedPath } from '../../i18n/utils';
+import { Languages } from 'lucide-react';
 
 interface Props {
   locale: any;
 }
 export const LanguagePicker: React.FC<Props> = ({ locale }) => {
   const translatePath = useTranslatedPath(locale);
+  const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <ul>
-      {Object.entries(languages).map(([lang, label]) => (
-        <li key={lang}>
-          <a href={translatePath('/', lang)}>{label}</a>
-        </li>
-      ))}
-    </ul>
+    <div className='relative'>
+      {/* Botón para abrir/cerrar el dropdown */}
+      <button
+        onClick={() => setOpen(!open)}
+        className="cursor-pointer flex items-center"
+        aria-expanded={open}
+        aria-label="Cambiar idioma"
+      >
+        <Languages color="#0A0A0A" />
+      </button>
+      {/* Dropdown */}
+      {open && (
+         <ul className='absolute'>
+        {Object.entries(languages).map(([lang, label]) => (
+          <li key={lang}>
+            <a href={translatePath('/', lang)}>{label}</a>
+          </li>
+        ))}
+      </ul>
+      )}
+
+    </div>
   )
 }
--- src/components/React/Navbar.tsx
+++ src/components/React/Navbar.tsx
@@ -1,14 +1,125 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Menu, X } from 'lucide-react'
 import { LanguagePicker } from './LanguagePicker';
 
 interface Props {
-  locale: any;
+  locale: any
 }
 
+/* Data Example */
+const data = [
+  {
+    id: 1,
+    title: 'Inicio',
+    url: '/',
+    children: []
+  },
+  {
+    id: 2,
+    title: 'Nosotros',
+    url: '/nosotros',
+    children: []
+  },
+  {
+    id: 3,
+    title: 'Servicios',
+    url: '/servicios',
+    children: [
+      { id: 1, title: 'Servicio 1', url: '/servicios/servicio-1' },
+      { id: 2, title: 'Servicio 2', url: '/servicios/servicio-2' },
+      { id: 3, title: 'Servicio 3', url: '/servicios/servicio-3' }
+    ]
+  },
+]
+
 export const Navbar: React.FC<Props> = ({ locale }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
-    <nav>
-      <LanguagePicker locale={locale} />
+    <nav className='fixed z-10 w-full bg-beige-50'>
+      <div className=" flex px-4 py-8 gap-4 justify-between items-center max-w-5xl mx-auto">
+        {/* Permite al usuario cambiar de idioma */}
+        <div className='inline-flex space-x-4'>
+          <LanguagePicker locale={locale} />
+          <h1>Logotipo</h1>
+
+        </div>
+
+        {/* Desktop Menu */}
+        <div className="hidden sm:inline-flex justify-start items-center gap-4 body">
+          {
+            data.map((item: any) => {
+              return (
+                <div key={item.id}>
+                  {item.children.length === 0 ?
+                    /* Link */
+                    <a href={item.url}>{item.title}</a>
+                    :
+                    /* Dropdown */
+                    <div key={item.id} className='relative group'>
+                      <span className='cursor-pointer'>{item.title}</span>
+                      <div className='absolute right-0 pt-4 hidden group-hover:block w-max'>
+                        <ul className="bg-beige-50 shadow-2xl rounded-xl px-4 py-6 space-y-3 text-right">
+                          {item.children.map((child: any) => {
+                            return (
+                              <li key={child.id}>
+                                <a href={child.url}>{child.title}</a>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  }
+                </div>
+              )
+            }
+            )
+          }
+        </div>
+
+        {/* Menu Button */}
+        <button onClick={() => setOpen(!open)} className='md:hidden'>
+          {!open ? <Menu color='#0A0A0A' /> : <X color='#0A0A0A' />}
+        </button>
+
+      </div>
+
+      {/* Mobile Menu */}
+      {open && (
+        <div className="px-6 pt-8 pb-16 bg-primary flex flex-col gap-y-2 text-3xl">
+          {
+            data.map((item: any) => {
+              return (
+                <div key={item.id}>
+                  {item.children.length === 0 ?
+                    /* Link */
+                    <a href={item.url}>{item.title}</a>
+                    :
+                    /* Dropdown */
+                    <div key={item.id} className='relative group'>
+                      <span className='cursor-pointer'>{item.title}</span>
+                      <div className='absolute right-0 pt-4 hidden group-hover:block w-max'>
+                        <ul className="bg-beige-50 shadow-2xl rounded-xl px-4 py-6 space-y-3 text-right">
+                          {item.children.map((child: any) => {
+                            return (
+                              <li key={child.id}>
+                                <a href={child.url}>{child.title}</a>
+                              </li>
+                            );
+                          })}
+                        </ul>
+                      </div>
+                    </div>
+                  }
+                </div>
+              )
+            }
+            )
+          }
+        </div>
+      )
+      }
     </nav>
   )
 }
