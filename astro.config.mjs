43423b57bef5fb40f8bb927eeba077a706919c35
bipolab-eu <bentagai@bipolab.eu>
2025-10-29 12:02:38 +0000

    mejoras ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,25 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
+  },
+
+  output: 'server',
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  i18n: {
+    locales: ["es", "en"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+    }
   }
 });
